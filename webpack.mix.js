286905d5de24080021ac3bf98cfb0291f9825dcd
Mevlut Canvar <mevlutcanvar@gmail.com>
2023-01-04 02:20:40 +0300

    v2.0.1

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.setPublicPath('dist');
-mix.setResourceRoot('/dist');
-
-mix.sass('src/travel-icons.scss', 'dist')
-    .minify('dist/travel-icons.css')
-    .copy('src/index.html', 'dist');
