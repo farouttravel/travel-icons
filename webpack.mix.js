5695f5e341547821385f539df2b1b87ffa817d22
Mevlut Canvar <mevlutcanvar@gmail.com>
2023-01-04 01:17:39 +0300

    adds minification of distributed styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@ let mix = require('laravel-mix');
 mix.setPublicPath('dist');
 mix.setResourceRoot('/dist');
 
-mix.sass('src/travel-icons.scss', 'dist');
+mix.sass('src/travel-icons.scss', 'dist')
+    .minify('dist/travel-icons.css');
